f9ba8e69eaedc5b22003f5f996545cd41151339e
Daniel Cronauer <dcronaue@gmail.com>
2025-10-14 18:38:58 -0400

    remade file package to name for our team

---
+++ Wolf_of_Wright_Street/src/main/java/com/wolf/budgetapp/WolfOfWrightStreetApplication.java
@@ -0,0 +1,14 @@
+package com.wolf.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfOfWrightStreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfOfWrightStreetApplication.class, args);
+	}
+
+}
+
