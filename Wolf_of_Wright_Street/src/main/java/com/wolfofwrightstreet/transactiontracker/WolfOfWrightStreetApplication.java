0d699218b8343ce1181fee8ece4fd5831b70eece
honeypint <kclondon22@yahoo.com>
2025-10-15 13:04:27 -0400

    Change package import to reflect name changes

--- Wolf_of_Wright_Street/src/main/java/com/wolfofwrightstreet/transactiontracker/WolfOfWrightStreetApplication.java
+++ Wolf_of_Wright_Street/src/main/java/com/wolfofwrightstreet/transactiontracker/WolfOfWrightStreetApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.wolfofwrightstreet.transactiontracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
