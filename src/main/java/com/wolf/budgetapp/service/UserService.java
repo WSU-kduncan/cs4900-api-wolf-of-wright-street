754129f52f1125810eb1d8460e4f43edac6f6ed9
cyrus60 <prodbycyrus@gmail.com>
2025-10-21 19:47:20 -0400

    updated getUserByLastName method in user service to implement Optional<>

--- src/main/java/com/wolf/budgetapp/service/UserService.java
+++ src/main/java/com/wolf/budgetapp/service/UserService.java
@@ -26,11 +26,11 @@ public class UserService {
     return result.get();
   }
 
-  public List<User> getUsersByLastName(String lastName) {
-    List<User> users = userRepository.findByLastName(lastName);
-    if (users.isEmpty()) {
-      throw new EntityNotFoundException("No users found with last name (" + lastName + ")");
+  public List<User> getUserBylastName(String lastName) {
+    Optional<List<User>> result = userRepository.findByLastName(lastName);
+    if (result.isEmpty()) {
+        throw new EntityNotFoundException("User (" + lastName + ") not found");
     }
-    return users;
+    return result.get();
   }
 }
