e51f27365d7fe3925d85410822b6fb4e9158d02b
honeypint <kclondon22@yahoo.com>
2025-11-04 12:46:21 -0500

    potentially fixed mappings

--- src/main/java/com/wolf/budgetapp/mapper/BudgetDtoMapper.java
+++ src/main/java/com/wolf/budgetapp/mapper/BudgetDtoMapper.java
@@ -2,6 +2,7 @@ package com.wolf.budgetapp.mapper;
 
 import com.wolf.budgetapp.dto.BudgetDto;
 import com.wolf.budgetapp.model.Budget;
+import com.wolf.budgetapp.model.TransactionCategory;
 import com.wolf.budgetapp.service.TransactionService;
 import com.wolf.budgetapp.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
@@ -14,14 +15,25 @@ import org.mapstruct.MappingTarget;
     componentModel = "spring",
     uses = {UserService.class, TransactionService.class, BudgetIdDtoMapper.class})
 public interface BudgetDtoMapper {
+  @Mapping(target = "user", source = "userEmail")
+  @Mapping(target = "category", source = "categoryName")
+  @Mapping(target = "id", source = "id")
   Budget toEntity(BudgetDto budgetDto) throws EntityNotFoundException;
 
+  @Mapping(target = "userEmail", source = "user.emailAddress")
+  @Mapping(target = "categoryName", source = "category.categoryName")
   BudgetDto toDto(Budget budget) throws EntityNotFoundException;
 
   List<BudgetDto> toDtoList(List<Budget> budgetList) throws EntityNotFoundException;
 
   @Mapping(target = "user", source = "userEmail")
-  @Mapping(target = "amount", source = "amount")
-  @Mapping(target = "category", ignore = true)
+  @Mapping(target = "category", source = "categoryName")
   void updateEntity(BudgetDto to, @MappingTarget Budget entity);
+
+  default TransactionCategory map(String categoryName) {
+    if (categoryName == null) return null;
+    TransactionCategory category = new TransactionCategory();
+    category.setCategoryName(categoryName);
+    return category;
+  }
 }
--- src/main/java/com/wolf/budgetapp/mapper/BudgetIdDtoMapper.java
+++ src/main/java/com/wolf/budgetapp/mapper/BudgetIdDtoMapper.java
@@ -9,12 +9,12 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BudgetIdDtoMapper {
+  @Mapping(target = "userEmail", source = "emailAddress")
   BudgetIdDto toDto(BudgetID budgetId) throws EntityNotFoundException;
 
+  @Mapping(target = "emailAddress", source = "userEmail")
   BudgetID toEntity(BudgetIdDto budgetIdDto) throws EntityNotFoundException;
 
   @Mapping(target = "emailAddress", source = "userEmail")
-  @Mapping(target = "categoryName", source = "categoryName")
-  @Mapping(target = "budgetPeriod", source = "budgetPeriod")
   void updateEntity(BudgetIdDto to, @MappingTarget BudgetID entity);
 }
